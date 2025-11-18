075184d1f9b41955c20641b1215a45a6d10c7e57
Lucy Ramplin <lucyramplin@gmail.com>
2025-11-18 20:24:58 +0000

    Clearing out default code and starting core build

---
+++ database/migrations/2025_11_18_200800_create_ingredients_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('ingredients', function (Blueprint $table) {
+            $table->id();
+            $table->unique('name');
+            $table->enum('type', ['flour', 'salt', 'seed', 'nut', 'confection', 'wet', 'spice', 'herb']);
+            $table->enum('unit', ['g', 'ml']);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('ingredients');
+    }
+};
